41de413c54006e415884ba92f7e5ea6fb37f5b67
maggo <marcokaul@gmail.com>
2019-08-25 01:08:21 +0200

    Initial viewport size

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,10 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 window.addEventListener("resize", () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
